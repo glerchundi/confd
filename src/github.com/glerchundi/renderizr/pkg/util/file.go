9fc0416b61b698ec6a4007d66e3f5776aaf1c6ca
Gorka Lerchundi Osa <glertxundi@gmail.com>
2015-11-20 13:20:17 +0100

    be more verbose with file not found errors

--- src/github.com/glerchundi/renderizr/pkg/util/file.go
+++ src/github.com/glerchundi/renderizr/pkg/util/file.go
@@ -62,21 +62,24 @@ func IsSameConfig(src, dest string) (bool, error) {
 
 // getFileInfo returns a FileInfo describing the named file.
 func getFileInfo(name string) (fi fileInfo, err error) {
-	if IsFileExist(name) {
-		f, err := os.Open(name)
-		if err != nil {
-			return fi, err
-		}
-		defer f.Close()
-		stats, _ := f.Stat()
-		fi.Uid = stats.Sys().(*syscall.Stat_t).Uid
-		fi.Gid = stats.Sys().(*syscall.Stat_t).Gid
-		fi.Mode = stats.Mode()
-		h := md5.New()
-		io.Copy(h, f)
-		fi.Md5 = fmt.Sprintf("%x", h.Sum(nil))
-		return fi, nil
-	} else {
-		return fi, fmt.Errorf("File not found")
+	if !IsFileExist(name) {
+		return fi, fmt.Errorf("%s file not found", name)
 	}
+
+	f, err := os.Open(name)
+	if err != nil {
+		return fi, err
+	}
+	defer f.Close()
+
+	stats, _ := f.Stat()
+	fi.Uid = stats.Sys().(*syscall.Stat_t).Uid
+	fi.Gid = stats.Sys().(*syscall.Stat_t).Gid
+	fi.Mode = stats.Mode()
+
+	h := md5.New()
+	io.Copy(h, f)
+	fi.Md5 = fmt.Sprintf("%x", h.Sum(nil))
+
+	return fi, nil
 }
